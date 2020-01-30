fd2e574611aedaf37d9133f7e9b33348c119f658
jphrubant <jphrubant@gmail.com>
2020-01-30 16:47:54 +0100

    swapped enemy for clokcs

--- Enemy.js
+++ Enemy.js
@@ -3,7 +3,7 @@
 function Enemy(canvas, x, speed){
     Book.call(this, canvas, x, speed)
     this.img = null;
-    this.size = 50;
+    this.size = 60;
     this.y = 0 - this.size;
 }
 
@@ -12,7 +12,7 @@ Enemy.prototype.constructor = Enemy;
 
 Enemy.prototype.create = function() { // Function to create the enemies on screen.
     this.img = new Image(); 
-    this.img.src = "images/dunce.png";
+    this.img.src = "images/clock.png";
     this.ctx.drawImage(this.img, this.x, this.y, this.size, this.size);
  };
 
